10b1293799fbef56b29462d5ff5ea3ba461532d9
todorbelic <belictodor@gmail.com>
2023-03-27 13:33:03 +0200

    initial model structure

---
+++ XMLApp/Model/Flight.cs
@@ -0,0 +1,12 @@
+﻿namespace XMLApp.Model
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public GeoLocation? TakeOffLocation { get; set; }
+        public GeoLocation? LandingLocation { get; set; }
+        public DateTime? TakeOffDate { get; set; }
+        public DateTime? LandingDate { get; set; }
+        public Ticket? Ticket { get; set; }
+    }
+}
---
+++ XMLApp/Model/Ticket.cs
@@ -0,0 +1,10 @@
+﻿namespace XMLApp.Model
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public Flight? Flight { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
